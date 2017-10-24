94c4951eefb1ba8e9dae8fccf4967861ea5560ad
Tori Klein <torikklein@gmail.com>
2017-10-23 19:32:44 -0500

    updating database

---
+++ main.py
@@ -0,0 +1,169 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+import cgi
+import os
+import jinja2
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:root@localhost:8889/blogz'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(240))
+    completed = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        self.completed = False
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    tasks = db.relationship('Task', backref='owner')
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.tasks = tasks
+
+@app.route('/blog', methods=['GET'])
+def index():
+
+    owner = User.query.filter_by(email=session['email']).first()
+
+    if request.method == 'POST':
+        task_name = request.form['task']   
+        new_task = Task(task_name, owner)
+        db.session.add(new_task)
+        db.session.commit()
+
+    tasks = Task.query.filter_by(completed=False,owner=owner).all()
+    completed_tasks = Task.query.filter_by(completed=True,owner=owner).all()
+    return render_template('todos.html', title="Blogs", 
+        tasks=tasks, completed_tasks=completed_tasks)
+    
+
+@app.route('/userlist', methods=['GET'])
+def listusers():
+    
+    users = User.query.all()
+    
+    return render_template('userlist.html',title="USERS", 
+        users=users)
+
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/blog')
+
+
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
+
+    body_error = ''
+    title_error = ''
+    blog_body = ''
+    blog_title = ''
+
+    if request.method == 'POST':
+        blog_title = request.form['blog-title']
+        blog_body = request.form['blog-body']
+
+        if blog_title == '':
+            title_error = "Enter a valid title"
+
+        if blog_body == '':
+            body_error = "Enter a valid body text"
+
+    
+        if title_error != '' or body_error != '':
+            return render_template('addblog.html', 
+            title_error=title_error, 
+            body_error=body_error,
+            blog_title=blog_title,
+            blog_body=blog_body)
+        else:
+            new_blog = Task(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
+
+        return redirect('/blog?id={0}'.format(new_blog.id))
+                    
+        
+    return render_template('addblog.html',title="BLOGS")
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['email'] = email
+            flash("Logged in")
+            return redirect('/newpost')
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # TODO - validate user's data
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+            return redirect('/blog')
+        else:
+            # TODO - user better response messaging
+            return "<h1>Duplicate user</h1>"
+
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/login')
+
+
+if __name__ == '__main__':
+    app.run()
